5baeb6a0ea6b61e671e72f09ed043c962dd06986
ygorrc <32312761+ygorrc@users.noreply.github.com>
2022-05-26 10:21:36 -0300

    Criação de algumas peças do chadrez
    Foi criado peça Rei e torre
    Criado programação defensiva
    Criado o Board

---
+++ src/application/UI.java
@@ -0,0 +1,26 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {
+	
+	public static void printBoard(ChessPiece[][] chessPiece) {
+		for(int i=0; i<chessPiece.length; i++) {
+			System.out.print((8-i) + " ");
+			for(int j=0; j<chessPiece.length; j++) {
+				 printPiece(chessPiece[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+	
+	private static void printPiece(ChessPiece piece) {
+		if(piece == null) {
+			System.out.print("-");
+		}else {
+			System.out.print(piece);
+		}
+		System.out.print(" ");
+	}
+}
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int colunms) {
+		if(rows < 1 || colunms < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.colunms = colunms;
 		pieces = new Piece[rows][colunms];
@@ -16,17 +19,45 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColunms() {
 		return colunms;
 	}
-
-	public void setColunms(int colunms) {
-		this.colunms = colunms;
+	
+	public Piece piece(int row , int colunm) {
+		if(!positionExists(row,colunm)) {
+			throw new BoardException("Positon not on the board");
+		}
+		return pieces[row][colunm];
 	}
 	
+	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Positon not on the board");
+		}
+		return pieces[position.getRow()][position.getColunm()]; 
+	}
+	
+	public void placePiece(Piece piece,Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is alredy a piece on pósition " + position);
+		}
+		pieces[position.getRow()][position.getColunm()] = piece;
+		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row>=0 && row< rows && column>=0 && column < colunms;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(),position.getColunm());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Positon not on the board");
+		}
+		return piece(position) != null;
+	}
 	
 }
