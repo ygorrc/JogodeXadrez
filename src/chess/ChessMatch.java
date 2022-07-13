6f25741cc319ad029a2ffaa29f107175d19a0bf6
ygorrc <32312761+ygorrc@users.noreply.github.com>
2022-07-13 14:49:00 -0300

    Mostrando Turno e player
    Implementado a impressão do player e do turno.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,15 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		
 		board = new Board(8,8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
 	
@@ -28,6 +32,13 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
 	public boolean[][] possibleMoves(ChessPosition sourcePosition){
 		 Position position = sourcePosition.toPosition();
 		 validateSourcePosition(position);
@@ -39,6 +50,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source,target);
 		Piece capturedPiece = makeMove(source,target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -54,6 +66,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("There chosen piece is not yours");
+		}
 		if(!board.piece(position).isTherePossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");         
 		}
@@ -63,6 +78,11 @@ public class ChessMatch {
 			throw new ChessException("There chosen piece can't move to target position");
 		}
 		
+	}
+	
+	private void nextTurn() {
+		turn ++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE  ;
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece,new ChessPosition(column,row).toPosition());
